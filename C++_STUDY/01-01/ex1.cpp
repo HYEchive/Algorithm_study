85d5bcc2b8abf3cb73fed9acbec0bc7151e0d0a2
sallyk0212 <sallyk0212@nate.com>
2021-12-28 14:50:59 +0900

    'test'

--- C++_STUDY/01-01/ex1.cpp
+++ C++_STUDY/01-01/ex1.cpp
@@ -5,8 +5,10 @@ int main(void)
     int num = 20;
     std::cout << "Hello World!" << std::endl;
     // std::cout은 c언어에서의 스트림 stdout에 해당
+
     std::cout << "Hello" << "World!" << std::endl;
     //std::endl 은 c언어에서의 '\n'에 해당
+    
     std::cout << num << ' ' << 'A';
     std::cout << ' ' << 3.14 << std::endl;
 
