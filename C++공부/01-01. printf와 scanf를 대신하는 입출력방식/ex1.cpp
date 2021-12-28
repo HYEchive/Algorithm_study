f1577df995990bd6346bb17fd53b3366ea2182f0
sallyk0212 <sallyk0212@nate.com>
2021-12-28 13:25:47 +0900

    practice

---
+++ C++공부/01-01. printf와 scanf를 대신하는 입출력방식/ex1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(void)
+{
+    int num = 20;
+    std::cout << "Hello World!" << std::endl;
+    // std::cout은 c언어에서의 스트림 stdout에 해당
+    std::cout << "Hello" << "World!" << std::endl;
+    //std::endl 은 c언어에서의 '\n'에 해당
+    std::cout << num << ' ' << 'A';
+    std::cout << ' ' << 3.14 << std::endl;
+
+    system("pause");    //vc++에서만 필요
+    return 0; 
+}
+
+/*
+Hello World!
+HelloWorld!
+20 A 3.14
+*/
